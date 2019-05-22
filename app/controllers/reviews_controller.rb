6bda6e9c02c3ae94f014c7e9376ec6cb48f6e439
Tyler McWilliam <tylermcwilliam@gmail.com>
2019-05-22 15:03:34 +0800

    COOOOOOL

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,12 +1,4 @@
 class ReviewsController < ApplicationController
-  def index
-    @reviews = Review.all
-  end
-
-  def show
-    @review = Review.find(params[:id])
-  end
-
   def new
     # we need @restaurant in our `simple_form_for`
     @restaurant = Restaurant.find(params[:restaurant_id])
@@ -17,28 +9,11 @@ class ReviewsController < ApplicationController
     @review = Review.new(review_params)
     @review.restaurant = Restaurant.find(params[:restaurant_id])
     @review.save
-    if @review.save == true
+    if @review.save
       redirect_to restaurant_path(@review.restaurant)
     else
       render :new
     end
-  end
-
-  def edit
-  end
-
-  def update
-    @review = Review.find(params[:id])
-    @review.update(review_params)
-
-    redirect_to reviews_path
-  end
-
-  def destroy
-    @review = Review.find(params[:id])
-    @review.destroy
-
-    redirect_to reviews_path
   end
 
   private
